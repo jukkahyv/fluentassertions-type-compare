a9622cc1ff8b0d45440f9ba2d8e7bf85e2df78a1
Jukka Snellman <jukkasne@gmail.com>
2024-12-03 18:49:51 +0200

    cleanup

--- FluentAssertionsTest/Tests.cs
+++ FluentAssertionsTest/Tests.cs
@@ -9,44 +9,28 @@ public class Tests
     {
         MyObj[] arr = [new("first"), new("second")];
         arr.Should().BeEquivalentTo(["first", "second"], options => options
-            //.WithMapping<string, MyObj>(e => e, s => s.Val)
             .Using<object>(ctx => ctx.Subject.Should().Be(ctx.Expectation.ToString()))
-            .When(_ => true)
+            .When(o => o.RuntimeType == typeof(MyObj) || o.RuntimeType == typeof(string))
         );
     }
 }
 
 class MyObj(string val) : IEquatable<string>, IEquatable<MyObj>
 {
-    public override bool Equals(object? obj)
+    public override bool Equals(object? obj) => obj switch
     {
-        if (obj is MyObj myObj)
-        {
-            return Equals(myObj);
-        }
+        MyObj myObj => Equals(myObj),
+        string str => Equals(str),
+        _ => false
+    };
 
-        if (obj is string str)
-        {
-            return Equals(str);
-        }
+    public bool Equals(MyObj? other) => val == other?.Val;
 
-        return false;
-    }
-
-    public bool Equals(MyObj? other)
-    {
-        return val == other?.Val;
-    }
-
-    public bool Equals(string? other)
-    {
-        return val == other;
-    }
+    public bool Equals(string? other) => val == other;
 
     public string Val => val;
 
-    public override string ToString()
-    {
-        return val;
-    }
+    public override string ToString() => val;
+
+    public override int GetHashCode() => val.GetHashCode();
 }
